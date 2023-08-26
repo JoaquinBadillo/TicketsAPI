a3d5bd38e33a1496cf6387b1062d5ac9bdc4c07c
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-08-25 23:39:24 -0600

    Decouple API from monorepo

--- client/src/components/tickets/utils.tsx
+++
@@ -1,6 +0,0 @@
-import { TextInput, ReferenceInput } from "react-admin";
-
-export const postFilters = [
-    <TextInput source="q" label="Search" alwaysOn />,
-    <ReferenceInput source="userId" label="User" reference="users" />
-];
