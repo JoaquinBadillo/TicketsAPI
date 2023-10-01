795f0fdbe0eb8c7b180b7427693e2b9f345af583
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-10-01 17:19:26 -0600

    Use ISO Week

--- routes/reports.js
+++ routes/reports.js
@@ -1,35 +1,32 @@
-const router = require('express').Router();
-const Ticket = require('../models/Ticket');
+const router = require("express").Router();
+const Ticket = require("../models/Ticket");
 
 // const authenticate = require('../middleware/auth');
 
-router.get('/', async (_req, res) => {
-    const currentDate = new Date();
-    const startDate = new Date(currentDate.getFullYear(), 0, 1);
-    const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
-    
-    const data = await Ticket
-    .find({
-        "$expr": { 
-            "$and": [
-                { "$eq": [{ "$week": "$date" }, Math.ceil(days / 7)] },
-                { "$eq": [{ "$year": "$date" }, currentDate.getFullYear()] }
-            ]
-        }
-    })
-    .select({ 
-        _id: 0, 
-        id: 1, 
-        status: 1, 
-        location: 1
-    });
+router.get("/", async (_req, res) => {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
 
-    if (!data)
-        return res.status(404).send({ message: 'No tickets this week' });
+  const data = await Ticket.find({
+    $expr: {
+      $and: [
+        { $eq: [{ $isoWeek: "$date" }, Math.ceil(days / 7)] },
+        { $eq: [{ $year: "$date" }, currentDate.getFullYear()] },
+      ],
+    },
+  }).select({
+    _id: 0,
+    id: 1,
+    status: 1,
+    location: 1,
+  });
 
-    res.set('Access-Control-Expose-Headers', 'Content-Range');
-    res.set('Content-Range', data.length);
-    res.send(data);
+  if (!data) return res.status(404).send({ message: "No tickets this week" });
+
+  res.set("Access-Control-Expose-Headers", "Content-Range");
+  res.set("Content-Range", data.length);
+  res.send(data);
 });
 
 module.exports = router;
