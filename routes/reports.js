db7203133da373a566a3bed2c25769db620f5661
Natalia Maya <nmaya0309@gmail.com>
2023-10-11 16:19:16 -0600

    route for location graph

--- routes/reports.js
+++ routes/reports.js
@@ -29,4 +29,29 @@ router.get("/", async (_req, res) => {
   res.send(data);
 });
 
+router.get("/location", async (_req, res) => {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+
+  const data = await Ticket.find({
+    $expr: {
+      $and: [
+        { $eq: [{ $isoWeek: "$date" }, Math.ceil(days / 7)] },
+        { $eq: [{ $year: "$date" }, currentDate.getFullYear()] },
+      ],
+    },
+  }).select({
+    _id: 0,
+    id: 1,
+    location: 1,
+  });
+
+  if (!data) return res.status(404).send({ message: "No tickets this week" });
+
+  res.set("Access-Control-Expose-Headers", "Content-Range");
+  res.set("Content-Range", data.length);
+  res.send(data);
+});
+
 module.exports = router;
