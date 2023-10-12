423fb90f4ed6c889b456b6915b82421c57ea51a6
Pboliopra <a01782428@tec.mx>
2023-10-11 20:12:45 -0600

    Los reportes ya andan tirando ruta

--- routes/reports.js
+++ routes/reports.js
@@ -3,7 +3,7 @@ const Ticket = require("../models/Ticket");
 
 // const authenticate = require('../middleware/auth');
 
-router.get("/", async (_req, res) => {
+router.get("/status", async (_req, res) => {
   const currentDate = new Date();
   const startDate = new Date(currentDate.getFullYear(), 0, 1);
   const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
@@ -20,6 +20,31 @@ router.get("/", async (_req, res) => {
     id: 1,
     status: 1,
     location: 1,
+  });
+
+  if (!data) return res.status(404).send({ message: "No tickets this week" });
+
+  res.set("Access-Control-Expose-Headers", "Content-Range");
+  res.set("Content-Range", data.length);
+  res.send(data);
+});
+
+router.get("/incidents", async (_req, res) => {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+
+  const data = await Ticket.find({
+    $expr: {
+      $and: [
+        { $eq: [{ $isoWeek: "$date" }, Math.ceil(days / 7)] },
+        { $eq: [{ $year: "$date" }, currentDate.getFullYear()] },
+      ],
+    },
+  }).select({
+    _id: 0,
+    id: 1,
+    category: 1,
   });
 
   if (!data) return res.status(404).send({ message: "No tickets this week" });
